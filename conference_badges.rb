48d34dbb4a94d033cc3c35fb8122f5fba2e49a86
Madison McCardle <madison.mccardle@powerhrg.com>
2020-04-29 22:11:45 -0400

    iteration more like confusion

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+name= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+attentes= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendees)
+        badges= []
+        attendees.each do |name|
+        badges.push("Hello, my name is #{name}.")
+      end
+    return badges
+end
+ 
+
+def assign_rooms(speaker)
+    rooms= []
+    speaker.each_with_index do |index, room|
+        rooms.push("Hello, #{index}! You'll be assigned to room #{room + 1}!")
+        end
+    return rooms 
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |results|
+        puts results
+    end
+    assign_rooms(attendees).each do |results|
+        puts results
+    end
+end 
+
+
